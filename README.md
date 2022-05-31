ffece0b54ae44c3cf283fe1e489d90f59a399156
yuki <yuki.onishi1129@gmail.com>
2022-05-31 16:17:18 +0900

    int型 double型

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 An absolute bare-bones web app.
+
+## コマンド実行
+
+```
+dart ファイル名
+
+例
+dart run datatype/int_double.dart
+```
---
+++ datatype/int_double.dart
@@ -0,0 +1,8 @@
+void main() {
+  // int 数値
+  // よく使われる例: ListView.builderやitemCount
+  // double 小数点を含む数値
+  // よく使われる例: Containerのwidthやheight
+
+  print(0.2 + 1.0);
+}
