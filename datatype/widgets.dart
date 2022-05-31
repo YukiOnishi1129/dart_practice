6b132d095c8a6b08eadd28e8f8501d14da8c5fbd
yuki <yuki.onishi1129@gmail.com>
2022-05-31 17:16:04 +0900

    widget

---
+++ datatype/widgets.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/material.dart';
+
+void main() {
+  // Widgets: FlutterでUIを構成するコンポーネント
+  // 文字を反映: Text Widget
+  // 箱を表示: Container Widget
+  // アイコンを表示: Icon WIdget
+
+  // Widgetsを使う場合は、「import 'package:flutter/material.dart';」をする必要がある
+
+  // Widget txt = Text('ユウキ');
+  // Widget container = Container(width: 10.0, height: 5.0);
+  // Widget icon = Icon(Icons.keyboard);
+  // Widget column = Column();
+}
