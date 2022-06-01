f12decf52c67ca0f666b77c6bb308df98e39ae75
yuki <yuki.onishi1129@gmail.com>
2022-06-01 14:25:01 +0900

    final & const

---
+++ modifier/final.dart
@@ -0,0 +1,14 @@
+void main() {
+  // final
+  // const
+  // 定数: 値を変更することができない
+  // 変数: 値を変更することができる
+
+  // finalとconstの違い
+  // const: コンパイル時定数
+  // finalには変数を入れられるが、constには入れられない
+
+  String name = '雄貴';
+  final String name2 = name;
+  // const String name3 = name;
+}
