4e051aaa9ff5a3163ef47a442b1fd3a0544b1b27
yuki <yuki.onishi1129@gmail.com>
2022-06-02 11:56:08 +0900

    for in

--- loop/loop.dart
+++ loop/loop.dart
@@ -1,7 +1,14 @@
 void main() {
   // for文
 
-  for (int i = 0; i < 5; i++) {
-    print(i);
+  // for (int i = 0; i < 5; i++) {
+  //   print(i);
+  // }
+
+  // for in
+  List<String> nameList = ['大西', '雄貴', 'いいお肉'];
+
+  for (String name in nameList) {
+    print(name);
   }
 }
